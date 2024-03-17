ebf52ad5b3cf8ffebece8c858bd842dfbfbb29e5
Fito Toledano <fito.toledano@gmail.com>
2024-03-17 10:34:00 +0100

    initial commit

---
+++ SmoothWalker/SmoothWalker/SmoothWalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothWalkerApp.swift
+//  SmoothWalker
+//
+//  Created by Fito Toledano on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SmoothWalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
